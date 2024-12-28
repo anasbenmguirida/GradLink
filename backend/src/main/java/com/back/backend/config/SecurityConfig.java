35c159263d1d6c3b38d2beb8de4cfba956491f15
MalikaTajidi <tajidimalika@gmail.com>
2024-12-28 13:29:41 +0100

    Add @NoArgsConstructor to Caummunaute entity for proper JSON serialization ig atkon tms7aat mli 3mlna merge

--- backend/src/main/java/com/back/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/back/backend/config/SecurityConfig.java
@@ -45,8 +45,8 @@ public class SecurityConfig {
             .requestMatchers(mvcMatcherBuilder.pattern("/api/login")).permitAll()
             .requestMatchers(mvcMatcherBuilder.pattern("/api/register")).permitAll()
             .requestMatchers(mvcMatcherBuilder.pattern("/ws")).permitAll()
-            .requestMatchers(mvcMatcherBuilder.pattern("/api/caummunautes")).permitAll()
-            .requestMatchers(mvcMatcherBuilder.pattern("/api/admin/caummunaute")).permitAll()
+            .requestMatchers(mvcMatcherBuilder.pattern("/messages")).permitAll()
+            .requestMatchers(mvcMatcherBuilder.pattern("/contacts/1")).permitAll()
             .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
