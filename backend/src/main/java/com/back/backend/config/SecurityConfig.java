fdfcc650607064730df2685d85a707e49c36db0f
MalikaTajidi <tajidimalika@gmail.com>
2024-12-31 16:40:37 +0100

    b4 merge

--- backend/src/main/java/com/back/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/back/backend/config/SecurityConfig.java
@@ -44,7 +44,6 @@ public class SecurityConfig {
                 .requestMatchers(mvcMatcherBuilder.pattern("/")).permitAll()
                 .requestMatchers(mvcMatcherBuilder.pattern("/api/login")).permitAll()
                 .requestMatchers(mvcMatcherBuilder.pattern("/api/register")).permitAll()
-                .requestMatchers(mvcMatcherBuilder.pattern("/api/searchuser")).permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session
